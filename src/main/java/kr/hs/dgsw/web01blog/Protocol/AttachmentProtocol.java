d82b59d5d7ff1f3649f95308710d3a08a43135ea
JEJ <eunjin10714@daum.net>
2019-04-22 13:26:54 +0900

    WEB_Blog

--- src/main/java/kr/hs/dgsw/web01blog/Protocol/AttachmentProtocol.java
+++ src/main/java/kr/hs/dgsw/web01blog/Protocol/AttachmentProtocol.java
@@ -1,7 +1,18 @@
 package kr.hs.dgsw.web01blog.Protocol;
 
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
 public class AttachmentProtocol {
+    private String storedPath;
+    private String originaName;
+
+
+    public AttachmentProtocol(String storedPath, String originaName) {
+        this.storedPath = storedPath;
+        this.originaName = originaName;
+    }
 }
